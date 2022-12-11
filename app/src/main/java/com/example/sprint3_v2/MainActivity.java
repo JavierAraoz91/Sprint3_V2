5061278baad84d04a9cd37bdc1827fbc4fe486dc
JavierAraoz <114873680+JavierAraoz@users.noreply.github.com>
2022-12-11 17:04:49 -0500

    modifRegistro-n1

--- app/src/main/java/com/example/sprint3_v2/MainActivity.java
+++ app/src/main/java/com/example/sprint3_v2/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        Map<Strin>
     }
 }
